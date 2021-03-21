ea86a18d1d39585f38c489c7d829656296054533
krzychug <krzychug>
2021-03-21 12:39:20 +0100

    Initial Commit

---
+++ PhotoFrame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by Krzysztof Gruszczynski on 21/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
